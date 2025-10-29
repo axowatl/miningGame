94d348d66393b139313669676629e698be4490bd
axowatl <117409439+axowatl@users.noreply.github.com>
2025-10-29 13:08:02 -0500

    Add Player class with initial properties

---
+++ js/player.js
@@ -0,0 +1,12 @@
+class Player {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.facing = 0;
+    // inventory
+    // upgrades
+    // stats
+  }
+}
