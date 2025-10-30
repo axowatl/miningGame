c4cd524ee75505ecca461e854eae3326d0972dcc
axowatl <117409439+axowatl@users.noreply.github.com>
2025-10-30 09:27:39 -0500

    push

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-
+/**
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.getElementById("canvas");
+const width = window.innerWidth;
+const height = window.innerHeight;
+canvas.width = width;
+canvas.height = height;
+/**
+ * @type {WebGL2RenderingContext}
+ */
+const gl = canvas.getContext('webgl2');
