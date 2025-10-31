f9dcb319af30a1f38e3dfb7c4802b27492f0cc79
axowatl <117409439+axowatl@users.noreply.github.com>
2025-10-31 10:55:14 -0500

    Render Image take 2

--- js/utility.js
+++ js/utility.js
@@ -59,26 +59,29 @@ export function createProgram(gl, vshader, fshader) {
 	return program;
 };
 
+// In utility.js (assuming you have a function like this)
 export function loadTexture(gl, url) {
     const texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
 
-    // Provide a temporary 1x1 blue pixel while the image loads to avoid errors
+    // Fill the texture with a 1x1 blue pixel while waiting for the image to load
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
 
     const image = new Image();
-    image.onload = function() {
+    image.onload = () => {
         gl.bindTexture(gl.TEXTURE_2D, texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
 
-        // Set texture parameters for general use, handling non-power-of-2 images
+        // **Add these lines to use nearest neighbor filtering for sharp pixels**
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+        
+        // Prevents blurring if the texture dimensions are not powers of 2
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
-        // Uncomment and use if your images are always power of 2 for better performance with mipmaps
-        // if (isPowerOf2(image.width) && isPowerOf2(image.height)) { gl.generateMipmap(gl.TEXTURE_2D); }
+        
+        // If your textures are powers of 2, you could use mipmaps instead:
+        // gl.generateMipmap(gl.TEXTURE_2D);
     };
     image.src = url;
 
